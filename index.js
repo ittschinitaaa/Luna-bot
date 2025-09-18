7b36d53c0b45af8905a7ec8e09b6375de5d00fa7
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-18 12:28:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n❀ Iniciando...'))
-say('Yuki Suou', {
+say('Luna Bot', {
 font: 'simple',
 align: 'left',
 gradient: ['green', 'white']
 })
-say('Made with love by Destroy', {
+say('Made with love by China', {
 font: 'console',
 align: 'center',
 colors: ['cyan', 'magenta', 'yellow']
