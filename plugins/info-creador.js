87a2f44a34da3d41857af42acd2cd190dc62e896
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-19 21:24:57 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn }) => {
   let name = await conn.getName(who);
 
   await sendContactArray(conn, m.chat, [
-    [`${suittag}`, `ᰔᩚ Propietario`, botname, `❀ No Hacer Spam`, `⊹˚• Venezuela •˚⊹`, md, bio],
+    [`${suittag}`, `ᰔᩚ Propietario`, botname, `❀ No Hacer Spam`, `⊹˚• Venezuela •˚⊹`, bio],
     [`${conn.user.jid.split('@')[0]}`, `✦ Es Un Bot`, packname, dev, `Sabra Dios 🫏`, channel, biobot]
   ], m);
 }
