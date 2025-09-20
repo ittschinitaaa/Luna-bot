c755523d0b6ce0d1de70aa32e1f54271152b593d
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-19 23:27:20 -0300

    Update group-settings.js

--- plugins/group-settings.js
+++ plugins/group-settings.js
@@ -2,7 +2,9 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 let isClose = { 'open': 'not_announcement', 'abrir': 'not_announcement', 'close': 'announcement', 'cerrar': 'announcement', }[command]
 await conn.groupSettingUpdate(m.chat, isClose)
 if (isClose === 'not_announcement') {
+await m.react('✔️')
 m.reply(`❀ *Ya pueden escribir en este grupo.*`)
+await m.react('✖️')
 } else if (isClose === 'announcement') {
 m.reply(`❀ *Sólo los admins pueden escribir en este grupo.*`)
 }}
